638915a2eba2bfad16fc64f36b090e1122193aba
Danny Yoo <dyoo@cs.wpi.edu>
2012-05-18 19:25:15 -0400

    cooking up a lang

---
+++ lang/lang.rkt
@@ -0,0 +1,16 @@
+#lang racket/base
+
+(require scribble/doclang
+         scribble/base
+         "../bootstrap.rkt"
+         (for-syntax racket/base))
+
+(provide (except-out (all-from-out scribble/doclang) #%module-begin)
+         (all-from-out "../bootstrap.rkt")
+         (all-from-out scribble/base)
+         (rename-out [module-begin #%module-begin]))
+
+(define-syntax (module-begin stx)
+  (syntax-case stx ()
+    [(_ id . body)
+     #`(#%module-begin id . body)]))
